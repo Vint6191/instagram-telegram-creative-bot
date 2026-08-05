1cc75ce3783d098104d12aaa04152f671430e53f
Vint6191 <134882345+Vint6191@users.noreply.github.com>
2026-08-05 20:35:49 +0300

    Delete telegram.ts

--- telegram.ts
+++
@@ -1,6 +0,0 @@
-import type { JobQueue } from "./job-queue";
-import type { Env } from "./types";
-
-export function queueStub(env: Env): JobQueue {
-  return env.QUEUE.getByName("instagram-creative-global-queue");
-}
